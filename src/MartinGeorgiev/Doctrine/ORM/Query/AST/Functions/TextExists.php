d7acc109aaa03188c17d3a9bcfc481b368eac225
Tac Tacelosky <tacman@gmail.com>
2024-04-24 09:44:04 -0400

    check for === instead of ?

--- src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/TextExists.php
+++ src/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/TextExists.php
@@ -7,7 +7,7 @@ namespace MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 /**
  * Implementation of PostgreSql check if a text string exists (using ?).
  *
- * @see https://www.postgresql.org/docs/9.5/functions-json.html
+ * @see https://www.postgresql.org/docs/current/functions-json.html#FUNCTIONS-JSONB-OP-TABLE
  *
  * @author Martin Georgiev <martin.georgiev@gmail.com>
  * @author Tac Tacelosky <tacman@gmail.com>
@@ -16,7 +16,7 @@ class TextExists extends BaseFunction
 {
     protected function customiseFunction(): void
     {
-        $this->setFunctionPrototype('(%s ? %s)');
+        $this->setFunctionPrototype('(%s == %s)');
         $this->addNodeMapping('StringPrimary');
         $this->addNodeMapping('StringPrimary');
     }
